e88ceca3f1000dbec6212f29e061ffc3507492c7
Saad Saif <saadgfx97@gmail.com>
2021-03-06 23:00:16 +0500

    moved stats from h1 to h2

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -3,25 +3,25 @@ import React from 'react'
 const Stats = ({ data }) => {
   return (
     <div className='stats'>
-      <h1>
+      <h2>
         <span role='img' aria-label='sheep'>
           😀
         </span>
         <span className='rocovered'>Recovered: {data.recovered.value}</span>
-      </h1>
-      <h1>
+      </h2>
+      <h2>
         <span role='img' aria-label='sheep'>
           😷
         </span>
         <span className='confirmed'>Confirmed: {data.confirmed.value}</span>
-      </h1>
+      </h2>
 
-      <h1>
+      <h2>
         <span role='img' aria-label='sheep'>
           😵
         </span>
         <span className='deaths'>Deaths: {data.deaths.value}</span>
-      </h1>
+      </h2>
     </div>
   )
 }
